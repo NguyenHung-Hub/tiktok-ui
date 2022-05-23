e4afdc144bbb784ac5b251ff5dc52e6c54a5c6a2
Hung <daminhnguyenhung@gmail.com>
2022-05-23 17:52:51 +0700

    ui header #2, #3

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://thumbs.dreamstime.com/b/lamborghini-reventon-white-chicago-wallpaper-42664449.jpg"
+                alt=""
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen van a</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCircleCheck} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
